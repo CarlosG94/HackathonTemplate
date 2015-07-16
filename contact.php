785b7687afa68916d93ab3fa77632c061fa5a05d
CarlosG94 <carlos.gtz.94@gmail.com>
2015-07-16 17:32:55 -0500

    added logo and other stuff

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@ if ( !defined( "PHP_EOL" ) ) define( "PHP_EOL", "\r\n" );
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-$address = "hi@yoarts.com";
+$address = "team@hacktheworld.io";
 
 // END OF Simple Configuration Options
 
